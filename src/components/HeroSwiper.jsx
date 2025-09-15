a8c7acf32e71e50dacf4b5e06cbdf8196e603df0
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-15 15:28:47 +0100

    change text hero animation

--- src/components/HeroSwiper.jsx
+++ src/components/HeroSwiper.jsx
@@ -1,6 +1,5 @@
 "use client";
-import Image from "next/image";
-
+import { useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,34 +19,44 @@ const palaceText = [
 ];
 
 const HeroSwiper = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <div className="absolute w-full flex justify-start items-center  z-50">
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper1"
-      >
-        {palaceText.map((palace, i) => (
-          <SwiperSlide key={i} className="">
-            <div className="text-white h-[60vh] text-[20.5px] uppercase w-full flex flex-col justify-between items-center">
-              <span>{""}</span>
-              <span>{palace.text}</span>
-              <Link
+    <div className="absolute w-full h-full  flex flex-col justify-center items-center py-20  z-50">
+      <div className="w-full h-full  flex justify-center items-center">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 6000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper1"
+          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+        >
+          {palaceText.map((palace, i) => (
+            <SwiperSlide key={i} className="">
+              <div className="text-white h-full text-[18.5px] tracking-widest uppercase w-full flex flex-col justify-between items-center">
+                <span>{""}</span>
+                <span>{palace.text}</span>
+                {/*<Link
                 href={palace.link}
                 className="text-[12px] bg-white text-black px-12 py-[6px] tracking-widest"
               >
                 {palace.linkText}
-              </Link>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+              </Link>*/}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <Link
+        href={"#"}
+        className="text-[13px] bg-white text-black px-12 py-[6px] tracking-widest uppercase"
+      >
+        {palaceText[activeIndex].linkText}
+      </Link>
     </div>
   );
 };
