58a85aa72e4106094965079e1a2aed5c63ee227b
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-16 05:21:38 +0100

    add new map

--- src/app/contact-us/components/Contact.jsx
+++ src/app/contact-us/components/Contact.jsx
@@ -50,14 +50,15 @@ const Contact = () => {
       </div>
 
       <div className="flex justify-center items-center w-full">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d849.3367170878846!2d-8.004560681687618!3d31.624351853949207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdafeef591e81187%3A0x5370e8aa4b3c1515!2sLe%20Palace!5e0!3m2!1sfr!2sma!4v1739352921203!5m2!1sfr!2sma"
-          height="500"
-          style={{ border: 0, width: "100%" }}
-          allowFullScreen=""
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <Link href="https://maps.app.goo.gl/uiT8cRKzyEr5nQ187">
+          <Image
+            src="/images/plan-1.png"
+            alt="Localisation Le Palace"
+            height={1000}
+            width={1000}
+            className="object-cover w-full"
+          />
+        </Link>
       </div>
     </div>
   );
