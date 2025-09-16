87679d27b634edd85c9618bb98c01de645d99b68
Youssef97dev <youssefmoussafi97@gmail.com>
2025-09-16 03:56:36 +0100

    add menu page

---
+++ src/app/menu/components/MenuFrame.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MenuFrame = () => {
+  return (
+    <iframe src="https://palace-menu.vercel.app/" className="w-full h-full" />
+  );
+};
+
+export default MenuFrame;
---
+++ src/app/menu/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MenuFrame from "./components/MenuFrame";
+
+const page = () => {
+  return (
+    <div className="h-screen overflow-y-auto">
+      <MenuFrame />
+    </div>
+  );
+};
+
+export default page;
